c95d233d6572e89eb239b0dd6e24b155e3632752
Nogs0 <joaoguinogueira04@gmail.com>
2024-03-06 23:46:24 -0300

    testes rating

--- src/components/StarsRating/index.tsx
+++ src/components/StarsRating/index.tsx
@@ -1,22 +1,31 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
-export default function StarsRating(stars: number) {
+export default function StarsRating(stars: any) {
+  const [estrelas, setEstrelas] = useState<number>(3);
+
+  const getStars = () => {
+    
+    // for(let i = 0; i < starsAux; i++)
+    if (estrelas > 1){
+      setEstrelas(estrelas - 1);
+      return 'star';
+    }
+    else if (estrelas > 0) return 'star-half-full';
+    
+    return 'star-outline'
+  };
+
+  const a1 = getStars();
   return (
     <View style={styles.container}>
-      <Icon name={'star'} size={50} color={'yellow'}></Icon>
-      <Icon name={'star'} size={50} color={'yellow'}></Icon>
-      <Icon name={'star'} size={50} color={'yellow'}></Icon>
-      <Icon name={'star-half-full'} size={50} color={'yellow'}></Icon>
-      <Icon name={'star-outline'} size={50} color={'yellow'}></Icon>
+      <Icon name={a1} size={50} color={'yellow'}></Icon>
+      <Icon name={getStars()} size={50} color={'yellow'}></Icon>
+      <Icon name={getStars()} size={50} color={'yellow'}></Icon>
+      <Icon name={getStars()} size={50} color={'yellow'}></Icon>
+      <Icon name={getStars()} size={50} color={'yellow'}></Icon>
     </View>
   )
 }
-
-// const getStars = (stars: number) => {
-  
-//   for(let i = 0; i < Math.floor(stars); i++)
-
-// }
