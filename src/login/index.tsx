0b3534053e9b5f8d3b778f00149c5e8e9f1c44fa
GreenShimada <matheus_jonnas@proton.me>
2024-05-26 16:12:02 -0300

    logo troca de cor

--- src/login/index.tsx
+++ src/login/index.tsx
@@ -3,28 +3,45 @@ import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
 import { useAuth } from '../contexts/auth';
 import style from './style';
 import LogoCliente from '../assets/images/@types/svg/LOGIN-CLIENTE.svg'
-
+import LogoProfissional from  '../assets/images/@types/svg/LOGIN-PROF.svg'
+import { useState  } from 'react';
+import {orangeDefault, blueDefault } from "../shared/styleConsts";
+import { View, Button } from 'react-native';
+import { blue } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
 
 export default function SignIn() {
     const { signIn } = useAuth();
-    
+    const [ color, setColor ] = useState('orange');
+    const [logo, setLogo] = useState(<LogoCliente style={style.logo} />);
+
+    const switchColor= () => {
+        if (color == blueDefault){
+            setColor(orangeDefault)
+            setLogo(<LogoCliente style={style.logo} />);
+        }else{
+            setColor(blueDefault)
+            setLogo(<LogoProfissional style={style.logo} />);
+        }
+        
+
+    }
+
     function handleSignIn(professional: boolean) {
         signIn(professional);
     }
 //  onPress={() => handleSignIn(true)} no botao de profissional
 //  onPress={() => handleSignIn(false)} no botao de cliente
+
     return (
         <SafeAreaView style={style.container}>
-            <LogoCliente style={style.logo}></LogoCliente>
-            <TouchableOpacity style={style.botao}
-               >
-                <Text style={style.text}>PROFISSIONAL</Text>
+            {logo}
+            <TouchableOpacity style={style.botao}>
+                <Text>PROFISSIONAL</Text>
 
             </TouchableOpacity>
-            <TouchableOpacity style={style.botao}
-                >
-                <Text style={style.text}>CLIENTE</Text>
-            </TouchableOpacity>
+
+
+            <Button title="Botão 1" color={color} onPress={switchColor} />
         </SafeAreaView>
     )
 }
