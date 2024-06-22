66feffe93976744deb54a05d2b9e68bcf089c8fc
GreenShimada <matheus_jonnas@proton.me>
2024-06-22 01:10:41 -0300

    Botao de whatsapp pronto

--- src/components/WhatsappButton/index.tsx
+++ src/components/WhatsappButton/index.tsx
@@ -5,9 +5,10 @@ import { toLinearSpace } from "react-native-reanimated/lib/typescript/reanimated
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { whiteDefault } from "../../shared/styleConsts";
 
+
 export default function WhatsappButton(props: any){
     return(
-        <TouchableOpacity style={style.WhatsappContainer} onPress={props.onPress}>
+        <TouchableOpacity  style={props.style} onPress={props.onPress}>
             <Icon name={"whatsapp"} size={25} color={whiteDefault} style={{marginRight: 5}}></Icon>
             <Text style={style.WhatsappButton}>{props.telefone}</Text>
         </TouchableOpacity>
--- src/components/WhatsappButton/style.tsx
+++ src/components/WhatsappButton/style.tsx
@@ -4,23 +4,17 @@ import WhatsappButton from ".";
 
 export default StyleSheet.create({
     WhatsappButton: {
+        
         marginTop: 3,
         fontSize: 14,
         color: whiteDefault,
-        fontFamily: 'Rubik-Regular'
+        fontFamily: 'Rubik-Regular',
+        
     },
-    WhatsappContainer: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginRight: "5%",
-        backgroundColor: greenWpp,
-        alignItems: 'center',
-        borderRadius: 50,
-        height: '20%',
-        width: "40%",
-        right: 0,
-        position: 'absolute'
-    },
+    
 
 })
+
+// import { Dimensions } from 'react-native'
+
+// const WindowWidth = Dimensions.get('window').width
